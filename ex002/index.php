29d6e3e6f255221de74fcb66e128a9d408c1c7e8
franklinlem <franklinlem@yahoo.com>
2023-08-13 11:34:34 -0300

    aula de php

--- ex002/index.php
+++ ex002/index.php
@@ -6,11 +6,27 @@
     <title>PHP</title>
 </head>
 <body>
-    <h1>Exemplo da PHP</h1>
+    <h1>Exemplo de PHP</h1>
         <?php 
             date_default_timezone_set("America/Sao_Paulo");
             echo "Hoje é dia " . date("d/m/Y D");
-            echo " e a hora é " . date("G:i:s")
+            echo " e a hora é " . date("G:i:s");
+            $sobrenome = "Martins";
+	        $nome = "Franklin";
+	        const PAIS = "Brasil";
+	        echo" Muito prazer, $nome $sobrenome! Você mora no " . PAIS;
+            #COMENTARIO EM PHP
+            //COMENTARIO EM PHP
+            $nomeCompletoCliente = "CamelCase";
+            $telefone_contato_fornecedor = "Snake Case";
+            $nomecursosuperior = "Tudo minusculo";
+            $_TX = 1234; //nome de variaveis podem ter simbolos
+            $£abc123 = 5678;
+            print $nomeCompletoCliente;
+            print $telefone_contato_fornecedor;
+            print $_TX;
+            print $£abc123;
+            
         ?>
     <p>Primeiro parágrafo de uma página com PHP.</p>
 </body>
