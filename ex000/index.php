4ba69d7a074227b3461a47db171f4156be3df316
franklinlem <franklinlem@yahoo.com>
2023-08-12 23:02:05 -0300

    aula de php

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira página com PHP</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Olá Mundo usando echo! \u{1f30e}";
+            print "Olá Mundo usando print!";
+        ?>
+    </h1>
+    <p>Primeiro parágrafo de uma página com PHP.</p>
+</body>
+</html>
