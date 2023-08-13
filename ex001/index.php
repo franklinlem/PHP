7708551ef93d269f9482f8ed8fd9f82ae358d168
franklinlem <franklinlem@yahoo.com>
2023-08-13 00:09:10 -0300

    aulas php

--- ex001/index.php
+++ ex001/index.php
@@ -6,11 +6,21 @@
     <title>Dados do Servidor</title>
 </head>
 <body>
-    <h1>Dados do Servidor
-        <?php 
-            phpinfo();
+    <h1>Dados do Servidor</h1>
+    <p> 
+        <?php
+            $nome = "Franklin Martins";
+            echo "O meu nome é $nome";
+            phpinfo()
         ?>
-    </h1>
+        <?php echo "supertag PHP" ?>
+        <? echo "short open tag" (nao funciona em alguns navegadores) ?>
+        <% "ASP tag" (nao funciona em alguns navegadores) %>
+        <?= "short tag PHP" ?>
+        <script language="php"> 
+            echo "<p>outra forma de usar PHP (nao funciona em alguns navegadores)</p>"
+        </script>
+    </p>
     <p>Primeiro parágrafo de uma página com PHP.</p>
 </body>
 </html>
