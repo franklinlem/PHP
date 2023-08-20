13ef8128d3aa57d8ed81532d4f6e7ee45b389405
franklinlem <franklinlem@yahoo.com>
2023-08-19 22:10:47 -0300

    aula de strings em php

---
+++ strings.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formatos de Strings</title>
+</head>
+<body>
+    <h1>FORMATO DE STRINGS</h1>
+    <p>
+    double quoted = aspas duplas ("Curso")
+    single quoted = aspas simples
+    Heredoc = 
+    Nowdoc =
+    Operador de concatenação = . ("Curso"."PHP" = "CursoPHP")
+    </p>
+    <?php 
+    
+    ?>
+</body>
+</html>
